25a1a8ec93005ba0c004030eea6e7d7658fc6f82
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-16 22:08:35 +0200

    Add progress to Github

--- chapter_8/make_pizza.py
+++ chapter_8/make_pizza.py
@@ -13,3 +13,21 @@ make_pizza('mushrooms', 'green peppers', 'extra cheese')
 # - mushrooms
 # - green peppers
 # - extra cheese
+
+
+def make_pizza2(size, *toppings):
+  """Summarize the pizza we are about to make."""
+  print(f"\nMaking a {size}-inch pizza with the following toppings:")
+  for topping in toppings:
+    print(f"- {topping}")
+
+make_pizza2(16, 'pepperoni')
+make_pizza2(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# Making a 16-inch pizza with the following toppings:
+# - pepperoni
+
+# Making a 12-inch pizza with the following toppings:
+# - mushrooms
+# - green peppers
+# - extra cheese
