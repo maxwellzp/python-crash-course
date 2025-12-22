bc03ee60b1e22de900a56ae070af520d44dd0cca
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-22 22:23:00 +0200

    Add progress to Github

---
+++ chapter_11/language_survey.py
@@ -0,0 +1,14 @@
+from survey import AnonymousSurvey
+
+question = "What language did you first learn to speak?"
+language_survey = AnonymousSurvey(question)
+
+print("Enter 'q' at any time to quit.\n")
+while True:
+  response = input("Language: ")
+  if response == 'q':
+    break
+  language_survey.store_response(response)
+
+print("\nThank you to everyone who participated in the survey!")
+language_survey.show_results()
