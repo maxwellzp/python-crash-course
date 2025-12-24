6b3039788c0f5dbbcd3791c62951c4d042dcbded
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-24 22:26:59 +0200

    Add progress to Github

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,6 +14,11 @@ class AlienInvasion:
     self.clock = pygame.time.Clock()
     self.settings = Settings()
 
+    # Fullscreen mode 
+    # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    # self.settings.screen_width = self.screen.get_rect().width
+    # self.settings.screen_height = self.screen.get_rect().height
+
     self.screen = pygame.display.set_mode(
       (self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
@@ -44,6 +49,8 @@ class AlienInvasion:
       self.ship.moving_right = True
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = True
+    elif event.key == pygame.K_q:
+      sys.exit()
     
   def _check_keyup_events(self, event):
     """Respond to key releases."""
