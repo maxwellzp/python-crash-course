ce9e66ccfb1700bc9960a021ec59962a2430474b
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-29 22:41:47 +0200

    Add progress to Github

--- plotting/mpl_squares4.py
+++ plotting/mpl_squares4.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-print(plt.style.available)
+# print(plt.style.available)
+# ['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'petroff10', 'seaborn-v0_8', 'seaborn-v0_8-bright', 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid', 'seaborn-v0_8-deep', 'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper', 'seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 'seaborn-v0_8-ticks', 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid', 'tableau-colorblind10']
+
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
