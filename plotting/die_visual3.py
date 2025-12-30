6cbb29034aebc7b1805ee71586ab798294ca364d
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-12-30 19:25:45 +0200

    Add progress to Github

---
+++ plotting/die_visual3.py
@@ -0,0 +1,26 @@
+import plotly.express as px
+
+from die import Die
+
+# Create a D6
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+  result = die.roll()
+  results.append(result)
+
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+  frequency = results.count(value)
+  frequencies.append(frequency)
+
+print(frequencies)
+# [168, 168, 147, 185, 161, 171]
+
+# Visualize the results. Making a Histogram
+fig = px.bar(x=poss_results, y=frequencies) 
+fig.show()
