f7b84fd73ebb4bc95cec7e937c36fcfa53a4d49c
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-01-08 10:24:34 +0200

    Add progress to Github

--- learning_log/ll_project/settings.py
+++ learning_log/ll_project/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps.
-    'learning_logs',   
+    'learning_logs',
+    'accounts',
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
