72a6146b57260fb39df710285e710d5eafee2101
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2026-01-09 12:44:00 +0200

    Add progress to Github

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -38,7 +38,9 @@ def new_topic(request):
     # POST data submitted; process data.
     form = TopicForm(data=request.POST)
     if form.is_valid():
-      form.save()
+      new_topic = form.save(commit=False)
+      new_topic.owner = request.user
+      new_topic.save()
       return redirect('learning_logs:topics')
   
   # Display a blank or invalid form.
@@ -70,6 +72,8 @@ def edit_entry(request, entry_id):
   """Edit an existing entry."""
   entry = Entry.objects.get(id=entry_id)
   topic = entry.topic
+  if topic.owner != request.user:
+    raise Http404
 
   if request.method != 'POST':
     # Initial request; pre-fill form with the current entry.
